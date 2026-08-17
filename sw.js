67822013f1ef74b634d4984e5163a6480df8b1ed
mahdiisa1388-svg <mahdi.isa1388@gmail.com>
2026-08-17 16:52:32 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'owl-habit-v2.33';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    if (new URL(event.request.url).origin === self.location.origin) caches.open(CACHE).then(c => c.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
